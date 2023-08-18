28dd4cea701aad8b82d90c96ed426c46a8284492
Out-A-Time <101604946+Out-A-Time@users.noreply.github.com>
2023-08-18 22:43:11 +0100

    Chapter 03.6. Styling the Task List section

--- TaskApp-Ranjan/App.js
+++ TaskApp-Ranjan/App.js
@@ -1,15 +1,47 @@
+import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState(["drink milk", "wash car"]);
+
+  function taskInputHandler(inputTask) {
+    setTask(inputTask);
+  }
+
+  function addNewTask() {
+    console.log("hello1");
+    setTaskList((currentTaskList) => {
+      return [...currentTaskList, task];
+    });
+  }
+
   return (
     <View style={styles.mainContainer}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Enter task details" style={styles.inputBox} />
-        <Button title="Add Task" style={styles.addTaskButton}></Button>
+        <TextInput
+          onChangeText={taskInputHandler}
+          placeholder="Enter task details"
+          style={styles.inputBox}
+        />
+        <Button
+          onPress={addNewTask}
+          title="Add Task"
+          style={styles.addTaskButton}
+        ></Button>
       </View>
-      <View style={styles.taskList}>
-        <Text>Your added Task</Text>
+      <View style={styles.taskListSection}>
+        <Text style={styles.taskOverviewTitle}>Your tasks:</Text>
+        {taskList.map((singleTask, index) => {
+          return (
+            <View style={styles.taskItemStyleView}>
+              <Text key={index} style={styles.taskItemStyleText}>
+                {index} {singleTask}
+              </Text>
+            </View>
+          );
+        })}
       </View>
 
       {/* <StatusBar style="auto" /> */}
@@ -42,10 +74,33 @@ const styles = StyleSheet.create({
     borderColor: "red",
     padding: 5,
     marginRight: 5,
+    backgroundColor: "white",
   },
   addTaskButton: {},
-  taskList: {
+  taskListSection: {
     flex: 5,
     backgroundColor: "green",
+  },
+  taskOverviewTitle: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  taskItemStyleView: {
+    margin: 5,
+    padding: 10,
+    borderWidth: 2,
+    borderRadius: 10,
+    borderColor: "black",
+    backgroundColor: "white",
+    // alignItems: "center",
+  },
+  taskItemStyleText: {
+    margin: 10,
+    padding: 10,
+    borderWidth: 2,
+    borderRadius: 10,
+    borderColor: "black",
+    backgroundColor: "yellow",
+    // alignItems: "center",
   },
 });
